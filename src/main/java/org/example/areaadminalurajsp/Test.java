39b06ddd2c87b60521e5da4447e892e119371a3b
guilhermevini2013 <guilhermevini2013@hotmail.com>
2024-01-13 13:18:45 -0300

    feito o metodo para deletar um curso na classe CourseConnection.java

--- src/main/java/org/example/areaadminalurajsp/Test.java
+++ src/main/java/org/example/areaadminalurajsp/Test.java
@@ -1,5 +1,6 @@
 package org.example.areaadminalurajsp;
 
+import org.example.areaadminalurajsp.connections.ConnectionInitializer;
 import org.example.areaadminalurajsp.connections.api.course.CourseConnection;
 import org.example.areaadminalurajsp.dtos.insert.CertificateDTO;
 import org.example.areaadminalurajsp.dtos.insert.CourseDTO;
@@ -10,11 +11,12 @@ import java.util.List;
 
 public class Test {
     public static void main(String[] args) throws IOException {
-        CourseConnection connection = new CourseConnection();
+        CourseConnection connection = new CourseConnection(new ConnectionInitializer());
         String token  = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJhdXRoLWFwaSIsInN1YiI6ImFkbWluQGFkbWluIiwiZXhwIjoxNzA1MTY2NjI4fQ.pTzMsQ30kxI2qpuj_rnySkzdCcU-agPrgd5EFjK1WEY";
 //        CourseDTO courseDTO = new CourseDTO("Curso em video222222222222","Curso em video222222222222", List.of(new VideoLessonDTO("javaaaaaaaaaa","youtube.com//",5)),
 //                new CertificateDTO("seilaaakfdaldadkdkldlakld"));
 //        connection.postCourse(courseDTO,token);
 //        System.out.println(connection.getAllCourseByFilter("","","2",token));
+        connection.deleteCourseByID(2l,token);
     }
 }
--- src/main/java/org/example/areaadminalurajsp/connections/api/course/CourseConnection.java
+++ src/main/java/org/example/areaadminalurajsp/connections/api/course/CourseConnection.java
@@ -11,27 +11,36 @@ import java.net.URI;
 import java.time.LocalDate;
 
 
-public class CourseConnection extends ConnectionInitializer {
+public class CourseConnection{
+    private ConnectionInitializer connection;
 
-    public String getCourseByID(Long id,String token) throws IOException {
+    public CourseConnection(ConnectionInitializer connection) {
+        this.connection = connection;
+    }
+
+    public String getCourseByID(Long id, String token) throws IOException {
         URI uriFindAllCourses = URI.create("http://localhost:8080/course/id/"+id);
-        CloseableHttpResponse response = doGetRequest(uriFindAllCourses,token);
+        CloseableHttpResponse response = connection.doGetRequest(uriFindAllCourses,token);
         return EntityUtils.toString(response.getEntity());
     }
     public String getAllCourse(Integer page,String token) throws IOException {
         URI uriFindAllCourse = URI.create("http://localhost:8080/course?pages="+page);
-        CloseableHttpResponse response = doGetRequest(uriFindAllCourse, token);
+        CloseableHttpResponse response = connection.doGetRequest(uriFindAllCourse, token);
         return EntityUtils.toString(response.getEntity());
     }
     public void postCourse(CourseDTO courseDTO,String token) throws IOException {
         URI uriPostCourse = URI.create("http://localhost:8080/course");
         Gson gson = new Gson();
         String json = gson.toJson(courseDTO);
-        doPostRequest(uriPostCourse,token,json);
+        connection.doPostRequest(uriPostCourse,token,json);
     }
     public String getAllCourseByFilter(String name,String date,String assessment,String token) throws IOException {
         URI uriAllCourseByFilter = URI.create("http://localhost:8080/course/filter?course="+name+"&publicationDate="+date+"&assessment="+assessment);
-        CloseableHttpResponse response = doGetRequest(uriAllCourseByFilter, token);
+        CloseableHttpResponse response = connection.doGetRequest(uriAllCourseByFilter, token);
         return EntityUtils.toString(response.getEntity());
+    }
+    public void deleteCourseByID(Long id,String token) throws IOException {
+        URI uriDeleteCourseByID = URI.create("http://localhost:8080/course/"+id);
+        connection.doDeleteRequest(uriDeleteCourseByID, token);
     }
 }
